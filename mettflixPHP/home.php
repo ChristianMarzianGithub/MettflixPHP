b1a82d9931a25db9cfa82d9cd8eacf1b326e1e26
randwerArbeit <marzian@innosoft.de>
2017-04-01 20:09:32 +0200

    initial commit

--- mettflixPHP/home.php
+++ mettflixPHP/home.php
@@ -1,10 +1,82 @@
-<!DOCTYPE html>
 <html>
-<body>
+	<head>
+		<title>Mettflix</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+		
+
+	<div id="link">
+		<div id="mainBox">	
+			<div id="subHeaderBox">
+				<div id="subHeader">					
+				</div>
+			</div>
+			<div id="subHeaderMenuBox">
+				<div id="subHeaderMenu">
+					<a href="index.html">frontpage</a>
+					<a href="home.php">home</a>						
+				</div>				
+			</div>	
+			<div id="subMainBox">
+				<div id="subMain">		
+					<?php
+					echo foo();					
+					connect();	
+					?>		
+					<video src=<?php echo foo()?> autoplay controls>
+					</video>
+				</div>				
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
 
 <?php
-echo "My first PHP script!";
-?>
+function foo()
+{
+    $retval = "\"media/videos/videoplayback.mp4\"";
+    return $retval;
+}
+function  connect(){
+$servername = "localhost";
+$username = "peter";
+$password = "asdf";
+$dbname = "test";
 
-</body>
-</html>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT name,pfad FROM movies";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table border=1>";
+    while($row = $result->fetch_assoc()) {
+    	echo "<tr>";
+    	echo "<td> " . $row["name"]. "</td>";
+    	echo "<td><a href=\""  . $row["pfad"]. "\">asdf</a></td>";
+    	echo "</tr>";
+
+
+
+        echo "id: " . $row["name"]. " - Name: " . $row["pfad"];
+
+
+
+
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+}
+?>
