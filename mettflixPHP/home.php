473807242529dc7bb761781aa745e994871627e9
randwerArbeit <marzian@innosoft.de>
2017-04-17 20:28:02 +0200

    included Bootstrap

--- mettflixPHP/home.php
+++ mettflixPHP/home.php
@@ -1,9 +1,17 @@
 <html>
 	<head>
+		 <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Mettflix</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 	</head>
-	<body >
+	<body>
+	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 		
 
 	<div id="link">
@@ -43,7 +51,7 @@ function foo()
 }
 function  connect(){
 $servername = "localhost";
-$username = "peter";
+$username = "a";
 $password = "asdf";
 $dbname = "test";
 
